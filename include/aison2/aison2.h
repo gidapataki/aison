1e3f694a9de09f1d10ef1630313b4a666be61339
Gida Pataki <gida.pataki@prezi.com>
2025-12-03 19:44:24 +0100

    Initial draft

--- include/aison2/aison2.h
+++ include/aison2/aison2.h
@@ -1,13 +1,291 @@
 #pragma once
 
-// Experimental v2 API for building schemas as objects instead of template
-// specializations. This is intentionally minimal for now and will be expanded
-// as the design settles.
-namespace aison2 {
+#include <tuple>
+#include <type_traits>
+#include <utility>
 
-struct SchemaDescriptor {
-  // Placeholder for future schema metadata; kept trivial so usage scaffolding
-  // compiles while we iterate on the API surface.
+namespace aison2 {
+namespace detail {
+
+// Simple typelist helpers ----------------------------------------------------
+template<class... Ts>
+struct TypeList {};
+
+template<class T, class List>
+struct Contains;
+
+template<class T, class... Ts>
+struct Contains<T, TypeList<Ts...>> : std::bool_constant<(std::is_same_v<T, Ts> || ...)> {};
+
+template<class ListA, class ListB>
+struct ConcatTwo;
+
+template<class... As, class... Bs>
+struct ConcatTwo<TypeList<As...>, TypeList<Bs...>> {
+    using type = TypeList<As..., Bs...>;
 };
+
+template<class... Lists>
+struct Concat;
+
+template<class List>
+struct Concat<List> {
+    using type = List;
+};
+
+template<class ListA, class ListB, class... Rest>
+struct Concat<ListA, ListB, Rest...> {
+    using type = typename Concat<typename ConcatTwo<ListA, ListB>::type, Rest...>::type;
+};
+
+template<class List>
+struct MakeUnique;
+
+template<>
+struct MakeUnique<TypeList<>> {
+    using type = TypeList<>;
+};
+
+template<class Head, class... Tail>
+struct MakeUnique<TypeList<Head, Tail...>> {
+private:
+    using TailUnique = typename MakeUnique<TypeList<Tail...>>::type;
+
+public:
+    using type = std::conditional_t<
+        Contains<Head, TailUnique>::value,
+        TailUnique,
+        typename ConcatTwo<TypeList<Head>, TailUnique>::type>;
+};
+
+template<class Needed, class Declared>
+struct AllContained;
+
+template<class Declared>
+struct AllContained<TypeList<>, Declared> : std::true_type {};
+
+template<class Head, class... Tail, class Declared>
+struct AllContained<TypeList<Head, Tail...>, Declared>
+    : std::conditional_t<
+          Contains<Head, Declared>::value,
+          AllContained<TypeList<Tail...>, Declared>,
+          std::false_type> {};
+
+// Dependencies only track class/enum types; fundamentals are treated as
+// built-ins that do not require schema declarations.
+template<class T>
+struct DependencyFor {
+    using type =
+        std::conditional_t<std::is_class_v<T> || std::is_enum_v<T>, TypeList<T>, TypeList<>>;
+};
+
+// Field plumbing -------------------------------------------------------------
+template<class Owner, class Field>
+struct FieldDef {
+    using OwnerType = Owner;
+    using FieldType = Field;
+    const char* name;
+    Field Owner::* ptr;
+};
+
+template<class... FieldDefs>
+struct FieldList {
+    std::tuple<FieldDefs...> fields;
+
+    constexpr explicit FieldList(FieldDefs... defs)
+        : fields(std::move(defs)...)
+    {}
+};
+
+template<class... FieldDefs>
+FieldList(FieldDefs...) -> FieldList<FieldDefs...>;
+
+template<class... FieldDefs>
+struct Fields : FieldList<FieldDefs...> {
+    using FieldList<FieldDefs...>::FieldList;
+};
+
+template<class... FieldDefs>
+Fields(FieldDefs...) -> Fields<FieldDefs...>;
+
+template<class Owner>
+struct ObjectContext {
+    template<class Field>
+    constexpr auto add(Field Owner::* ptr, const char* name) const
+    {
+        return FieldDef<Owner, Field>{name, ptr};
+    }
+};
+
+template<class FieldDefs>
+struct FieldDependencies;
+
+template<class... FieldDefs>
+struct FieldDependencies<FieldList<FieldDefs...>> {
+private:
+    template<class FieldDefT>
+    using Dependency = typename DependencyFor<typename FieldDefT::FieldType>::type;
+
+public:
+    using type =
+        typename MakeUnique<typename Concat<TypeList<>, Dependency<FieldDefs>...>::type>::type;
+};
+
+template<class... FieldDefs>
+struct FieldDependencies<Fields<FieldDefs...>> : FieldDependencies<FieldList<FieldDefs...>> {};
+
+// Enum plumbing --------------------------------------------------------------
+template<class T>
+struct EnumValue {
+    const char* name;
+    T value;
+};
+
+template<class... Values>
+struct EnumValues {
+    std::tuple<Values...> values;
+
+    constexpr explicit EnumValues(Values... v)
+        : values(std::move(v)...)
+    {}
+};
+
+template<class... Values>
+EnumValues(Values...) -> EnumValues<Values...>;
+
+template<class T>
+struct EnumContext {
+    constexpr auto value(const char* name, T v) const { return EnumValue<T>{name, v}; }
+};
+
+// Def traits -----------------------------------------------------------------
+template<class T, class Fields>
+struct ObjectDef {
+    using Type = T;
+    using FieldsType = Fields;
+    using Deps = typename FieldDependencies<FieldsType>::type;
+
+    FieldsType fields;
+};
+
+template<class T, class Values>
+struct EnumDef {
+    using Type = T;
+    using ValuesType = Values;
+    using Deps = TypeList<>;
+
+    ValuesType values;
+};
+
+template<class T>
+struct DeclareDef {
+    using Type = T;
+    using Deps = TypeList<>;
+};
+
+template<class Def>
+struct DefTraits;
+
+template<class T, class Fields>
+struct DefTraits<ObjectDef<T, Fields>> {
+    using Type = T;
+    using Deps = typename ObjectDef<T, Fields>::Deps;
+    static constexpr bool isDeclare = false;
+};
+
+template<class T, class Values>
+struct DefTraits<EnumDef<T, Values>> {
+    using Type = T;
+    using Deps = typename EnumDef<T, Values>::Deps;
+    static constexpr bool isDeclare = false;
+};
+
+template<class T>
+struct DefTraits<DeclareDef<T>> {
+    using Type = T;
+    using Deps = typename DeclareDef<T>::Deps;
+    static constexpr bool isDeclare = true;
+};
+
+template<class Def>
+using TypeFromDef = std::
+    conditional_t<DefTraits<Def>::isDeclare, TypeList<>, TypeList<typename DefTraits<Def>::Type>>;
+
+template<class Def>
+using DeclaredFromDef = TypeList<typename DefTraits<Def>::Type>;
+
+template<class Def>
+using DepsFromDef = typename DefTraits<Def>::Deps;
+
+}  // namespace detail
+
+// Public API -----------------------------------------------------------------
+template<class T, class F>
+constexpr auto Object(F&& fn)
+{
+    detail::ObjectContext<T> ctx;
+    auto fields = fn(ctx);
+    using FieldsType = std::decay_t<decltype(fields)>;
+    return detail::ObjectDef<T, FieldsType>{std::move(fields)};
+}
+
+template<class T, class F>
+constexpr auto Enum(F&& fn)
+{
+    detail::EnumContext<T> ctx;
+    auto values = fn(ctx);
+    using ValuesType = std::decay_t<decltype(values)>;
+    return detail::EnumDef<T, ValuesType>{std::move(values)};
+}
+
+template<class T>
+constexpr auto Declare()
+{
+    return detail::DeclareDef<T>{};
+}
+
+template<class... Defs>
+struct SchemaDef {
+    using DefinedTypes = typename detail::MakeUnique<
+        typename detail::Concat<detail::TypeList<>, detail::TypeFromDef<Defs>...>::type>::type;
+
+    using DeclaredTypes = typename detail::MakeUnique<
+        typename detail::Concat<DefinedTypes, detail::DeclaredFromDef<Defs>...>::type>::type;
+
+    using DepTypes = typename detail::MakeUnique<
+        typename detail::Concat<detail::TypeList<>, detail::DepsFromDef<Defs>...>::type>::type;
+
+    static_assert(
+        detail::AllContained<DepTypes, DeclaredTypes>::value,
+        "Schema is missing definitions or declarations for referenced types");
+
+    std::tuple<Defs...> defs;
+
+    template<class T>
+    static constexpr bool defines()
+    {
+        return detail::Contains<T, DefinedTypes>::value;
+    }
+
+    template<class T>
+    static constexpr bool declares()
+    {
+        return detail::Contains<T, DeclaredTypes>::value;
+    }
+};
+
+template<class... Defs>
+SchemaDef(Defs...) -> SchemaDef<Defs...>;
+
+template<class... Defs>
+constexpr auto Schema(Defs&&... defs)
+{
+    return SchemaDef<std::decay_t<Defs>...>{
+        std::tuple<std::decay_t<Defs>...>{std::forward<Defs>(defs)...}};
+}
+
+using detail::EnumValue;
+using detail::EnumValues;
+using detail::Fields;
 
 }  // namespace aison2
--- test/aison2_experiments.cpp
+++ test/aison2_experiments.cpp
@@ -4,7 +4,67 @@
 
 #include "aison2/aison2.h"
 
-TEST_CASE("aison2: scaffold compiles") {
-  aison2::SchemaDescriptor schema{};
-  CHECK(&schema != nullptr);
+namespace {
+
+struct Bar {
+    int x;
+};
+
+struct Foo {
+    int y;
+    Bar bar;
+};
+
+enum class Mode { Dark, Light, Automatic };
+
+struct External {
+    int z;
+};
+
+struct UsesExternal {
+    External ext;
+};
+
+}  // namespace
+
+TEST_CASE("aison2: schema scaffolding captures definitions and declarations")
+{
+    auto barDef = aison2::Object<Bar>([](auto& ctx) {
+        return aison2::Fields{
+            ctx.add(&Bar::x, "x"),
+        };
+    });
+
+    auto fooDef = aison2::Object<Foo>(
+        [](auto& ctx) { return aison2::Fields{ctx.add(&Foo::y, "y"), ctx.add(&Foo::bar, "bar")}; });
+
+    auto modeDef = aison2::Enum<Mode>([](auto& ctx) {
+        return aison2::EnumValues{
+            ctx.value("dark", Mode::Dark),
+            ctx.value("light", Mode::Light),
+            ctx.value("auto", Mode::Automatic),
+        };
+    });
+
+    auto usesExternalDef = aison2::Object<UsesExternal>([](auto& ctx) {
+        return aison2::Fields{
+            ctx.add(&UsesExternal::ext, "ext"),
+        };
+    });
+
+    auto schema = aison2::Schema(
+        aison2::Declare<External>(),              //
+        barDef, fooDef, modeDef, usesExternalDef  //
+    );
+
+    static_assert(decltype(schema)::template defines<Bar>(), "Bar should be defined");
+    static_assert(decltype(schema)::template defines<Foo>(), "Foo should be defined");
+    static_assert(decltype(schema)::template defines<Mode>(), "Mode should be defined");
+    static_assert(!decltype(schema)::template defines<External>(), "External is only declared");
+    static_assert(decltype(schema)::template declares<External>(), "External should be declared");
+
+    CHECK(schema.defines<Bar>());
+    CHECK(schema.defines<Foo>());
+    CHECK(schema.defines<Mode>());
+    CHECK(schema.declares<External>());
 }
